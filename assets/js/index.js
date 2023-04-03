7d4c45794c85e2f7d9f392b5fadea1ea200685ce
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-03 17:54:58 -0400

    add about btn page scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,9 +2,16 @@
 
 const contactPage = document.getElementById("contact-page");
 const contactBtn = document.querySelector(".contact-button");
+const aboutPage = document.getElementById("about-page");
+const aboutBtn = document.querySelector(".about-button");
 
 // Event Listeners
 contactBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  contactPage.scrollIntoView();
+  contactPage.scrollIntoView({ behavior: "smooth" });
+});
+
+aboutBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  aboutPage.scrollIntoView({ behavior: "smooth" });
 });
