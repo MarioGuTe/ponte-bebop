c9b16563694217cef07e5c5786f4ac063c402f43
MarioGuTe <mariogutierreztello@gmail.com>
2023-04-03 18:08:53 -0400

    add scroll to top by clicking nav title

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,18 @@
 // Selectors
-
+const navTitle = document.querySelector(".nav-title-container");
+const topPage = document.getElementById("top-page");
 const contactPage = document.getElementById("contact-page");
 const contactBtn = document.querySelector(".contact-button");
 const aboutPage = document.getElementById("about-page");
 const aboutBtn = document.querySelector(".about-button");
 
 // Event Listeners
+
+navTitle.addEventListener("click", (e) => {
+  e.preventDefault();
+  topPage.scrollIntoView({ behavior: "smooth" });
+});
+
 contactBtn.addEventListener("click", (e) => {
   e.preventDefault();
   contactPage.scrollIntoView({ behavior: "smooth" });
